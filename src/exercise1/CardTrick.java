1a56be27d1a85214d3efe4369d116ad6314630ab
Rachel Prasad <prasadra@sheridancollege.ca>
2023-02-08 16:43:58 -0500

    first commit

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,7 +1,7 @@
 package exercise1;
 
-import static java.lang.Math.random;
-
+import java.util.Random;
+import java.util.Scanner;
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -33,6 +33,37 @@ public class CardTrick {
         
         // If the guess is successful, invoke the printInfo() method below.
         
+        hand[0]= new Card();
+        hand[0].setValue(1);
+        hand[0].setSuit("Hearts");        
+        hand[1]= new Card();
+        hand[1].setValue(2);
+        hand[1].setSuit("Clubs");        
+        hand[2]= new Card();
+        hand[2].setValue(3);
+        hand[2].setSuit("Diamonds");
+        hand[3]= new Card();
+        hand[3].setValue(4);
+        hand[3].setSuit("Spades");
+        hand[4]= new Card();
+        hand[4].setValue(5);
+        hand[4].setSuit("Clubs");
+        hand[5]= new Card();
+        hand[5].setValue(12);
+        hand[5].setSuit("Hearts");
+        hand[6]= new Card();
+        hand[6].setValue(11);
+        hand[6].setSuit("Hearts");
+       
+        Scanner input = new Scanner(System.in);
+        int userValue =input.nextInt();
+        String userSuit= input.nextLine();
+        for(int i =0;i<hand.length;i++){
+            if(hand[i].getValue()==userValue &&
+               hand[i].getSuit().equals(userSuit)){
+                printInfo();
+            }//end of if
+        }//end of for
     }
 
     /**
