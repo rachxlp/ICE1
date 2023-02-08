c712541a3b0b62664ff68a9d921af852504ebf2c
Rachel Prasad <prasadra@sheridancollege.ca>
2023-02-08 15:58:00 -0500

    first commit

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,12 +1,13 @@
 package exercise1;
 
+import static java.lang.Math.random;
+
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Rachel Prasad February 2023
  */
 public class CardTrick {
     
@@ -17,10 +18,9 @@ public class CardTrick {
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
             //card.setValue(insert call to random number generator here)
-            // 
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
+            
         }
 
         // insert code to ask the user for Card value and suit, create their card
